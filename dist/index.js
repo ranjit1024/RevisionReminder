4ec95fbbbc68ba3a2de7d3972fbca7e7bb86da45
ranjit <ranjitdas2048@gmail.com>
2025-08-21 09:33:17 +0530

    added to redis

--- dist/index.js
+++ dist/index.js
@@ -12,19 +12,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
 const redis_1 = require("@upstash/redis");
-const client_s3_1 = require("@aws-sdk/client-s3");
-const client_ses_1 = require("@aws-sdk/client-ses");
 const redis = redis_1.Redis.fromEnv();
-const s3 = new client_s3_1.S3Client({
-    region: String(process.env.AWS_REGION),
-    credentials: {
-        accessKeyId: String(process.env.AWS_ACCESS_KEY_ID),
-        secretAccessKey: String(process.env.AWS_SECRET_ACCESS_KEY),
-    },
-    maxAttempts: 5,
-    retryMode: 'adaptive'
-});
-const sesClient = new client_ses_1.SESClient({ region: String(process.env.AWS_REGION) });
 function PutSessionReminderInquque() {
     return __awaiter(this, void 0, void 0, function* () {
         const revisionData = yield prisma.revision.findMany({
@@ -35,46 +23,9 @@ function PutSessionReminderInquque() {
                 email: true
             },
         });
-        // const sendRevisionReminder = await redis.lpush('reminder', JSON.stringify(revisionData));
+        redis.lpush('reminder', JSON.stringify(revisionData));
         console.log(revisionData);
         return revisionData;
     });
 }
 PutSessionReminderInquque();
-const createSendEmailCommand = () => __awaiter(void 0, void 0, void 0, function* () {
-    return new client_ses_1.SendEmailCommand({
-        Destination: {
-            ToAddresses: ["ranjitdas2048@gmail.com"]
-        },
-        Message: {
-            Body: {
-                Html: {
-                    Charset: "UTF-8",
-                    Data: "<h1>remider</h1>",
-                },
-                Text: {
-                    Charset: 'UTF-8',
-                    Data: 'fasdf'
-                },
-            },
-            Subject: {
-                Charset: 'UTF-8',
-                Data: "Reminder for Subject"
-            }
-        },
-        Source: 'ranjitdas3048@gmail.com'
-    });
-});
-function run() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const comand = yield createSendEmailCommand();
-            const result = yield sesClient.send(comand);
-            console.log(result);
-        }
-        catch (err) {
-            console.log(err);
-        }
-    });
-}
-run();
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 import { Redis } from "@upstash/redis";
-import { S3Client } from "@aws-sdk/client-s3"
-import { SendEmailCommand, SESClient } from "@aws-sdk/client-ses"
 const redis = Redis.fromEnv()
 
-
-const s3 = new S3Client({
-    region: String(process.env.AWS_REGION),
-    credentials: {
-        accessKeyId: String(process.env.AWS_ACCESS_KEY_ID),
-        secretAccessKey: String(process.env.AWS_SECRET_ACCESS_KEY),
-    },
-    maxAttempts: 5,
-    retryMode: 'adaptive'
-
-})
-const sesClient = new SESClient({ region: String(process.env.AWS_REGION) });
 async function PutSessionReminderInquque(): Promise<{
     time: Date;
     id: string,
@@ -32,45 +18,8 @@ async function PutSessionReminderInquque(): Promise<{
         },
 
     });
-    // const sendRevisionReminder = await redis.lpush('reminder', JSON.stringify(revisionData));
+    redis.lpush('reminder', JSON.stringify(revisionData))
     console.log(revisionData)
     return revisionData;
 }
 PutSessionReminderInquque()
-const createSendEmailCommand = async () => {
-    return new SendEmailCommand({
-        Destination: {
-            ToAddresses: ["ranjitdas2048@gmail.com"]
-        },
-        Message: {
-            Body: {
-                Html: {
-                    Charset: "UTF-8",
-                    Data: "<h1>remider</h1>",
-                },
-                Text:{
-                    Charset:'UTF-8',
-                    Data:'fasdf'
-                },
-            
-            },
-            Subject:{
-                Charset:'UTF-8',
-                Data:"Reminder for Subject"
-            }
-        },
-        Source:'ranjitdas3048@gmail.com'
-    })
-}
-
-async function run(){
-    try{
-            const comand = await createSendEmailCommand()
-         const result = await sesClient.send(comand);
-         console.log(result)
-    }
-    catch(err){
-        console.log(err)
-    }
-}
-run()
